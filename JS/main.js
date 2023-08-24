c9ebe141428c325a49d94ded109fbb0b2e9dd5d7
FrancescoPepi <francescopepi16@gmail.com>
2023-08-24 09:57:04 +0200

    fix end timer

--- JS/main.js
+++ JS/main.js
@@ -2,29 +2,42 @@
 const secondsBox = document.getElementById("seconds");
 const minutesBox = document.getElementById("minutes");
 const hoursBox = document.getElementById("hours");
-
+// data da ragiungere
 let tomorrowDate = new Date("2023-08-24 9:30").getTime();
+
+let secondsNumber = 1000;
+let minutesNumber = secondsNumber * 60;
+let HoursNumber = minutesNumber * 60;
+let dayNumbers = HoursNumber * 24;
 let countDown = setInterval(function () {
+  calctimer();
+}, 1000);
+
+// funzione che che calcola i secondi,
+const calctimer = () => {
   let nowDate = new Date().getTime();
   let distance = tomorrowDate - nowDate;
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  seconds < 10
-    ? (secondsBox.innerText = "0" + seconds)
-    : (secondsBox.innerText = seconds);
-
-  minutes < 10
-    ? (minutesBox.innerText = "0" + minutes)
-    : (minutesBox.innerText = minutes);
-
-  hours < 10
-    ? (hoursBox.innerText = "0" + hours)
-    : (hoursBox.innerText = hours);
 
   if (distance < 0) {
+    secondsBox.innerText = "00";
+    minutesBox.innerText = "00";
+    hoursBox.innerText = "00";
     clearInterval(countDown);
   }
-}, 1000);
+  const seconds = Math.floor((distance % minutesNumber) / secondsNumber);
+  const minutes = Math.floor((distance % HoursNumber) / minutesNumber);
+  const hours = Math.floor((distance % dayNumbers) / HoursNumber);
+  if (!distance < 0) {
+    seconds < 10
+      ? (secondsBox.innerText = "0" + seconds)
+      : (secondsBox.innerText = seconds);
+
+    minutes < 10
+      ? (minutesBox.innerText = "0" + minutes)
+      : (minutesBox.innerText = minutes);
+
+    hours < 10
+      ? (hoursBox.innerText = "0" + hours)
+      : (hoursBox.innerText = hours);
+  }
+};
